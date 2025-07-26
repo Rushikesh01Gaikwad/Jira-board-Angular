e58cd80032ea14fe68e96a9a6403a45a59bbd17d
Rushikesh Gaikwad <rushikeshhtc@gmail.com>
2025-07-26 08:31:49 +0530

    data in pro

--- src/app/component/projectjson.service.ts
+++ src/app/component/projectjson.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {Projectinterface} from './projectinterface'
+import { Projectinterface } from './projectinterface'
 import { map, Observable } from 'rxjs';
 
 @Injectable({
@@ -8,26 +8,33 @@ import { map, Observable } from 'rxjs';
 })
 export class ProjectjsonService {
 
-  constructor(private httpClient : HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
 
-  getAll()
-  {
-    return this.httpClient.get<Projectinterface[]>('http://localhost:3000/projectData');
+  //private baseUrl = 'http://localhost:3000/projectData';
+  private baseUrl = 'http://localhost:5227/swagger/api';
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      //'Authorization': Bearer ${this.provider.token}
+    })
+  };
+
+  getAll() {
+    return this.httpClient.get<Projectinterface[]>(`${this.baseUrl}/Get`, this.httpOptions);
   }
 
-  add(record: Projectinterface)
-  {
-    return this.httpClient.post('http://localhost:3000/projectData', record);
+  add(record: Projectinterface) {
+    return this.httpClient.post(`${this.baseUrl}/Insert`, record, this.httpOptions);
   }
 
-  update(data: any) 
-  {
-    return this.httpClient.put<Projectinterface>('http://localhost:3000/projectData/' + data.id, data);
+  update(data: any) {
+    //return this.httpClient.put<Projectinterface>('http://localhost:3000/projectData/' + data.id, data);
+    return this.httpClient.put<Projectinterface>(`${this.baseUrl}/Update` + data.id, data, this.httpOptions);
   }
 
-  delete(data :any)
-  {
-    return this.httpClient.delete(`http://localhost:3000/projectData/${data}`);
+  delete(data: any) {
+    //return this.httpClient.delete(`http://localhost:3000/projectData/${data}`);
+    return this.httpClient.delete(`${this.baseUrl}/delete/${data.id}`, this.httpOptions);
   }
 
 }
