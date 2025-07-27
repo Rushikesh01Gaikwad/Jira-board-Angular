bdc4dccbaa09626c2ffc9a321c5cf47ec21fd159
Rushikesh Gaikwad <rushikeshhtc@gmail.com>
2025-07-27 16:00:06 +0530

    login in process

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProjectjsonService } from '../projectjson.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,59 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  isLogin = true;
+  loginForm: FormGroup;
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private http: ProjectjsonService) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  toggleForm() {
+    this.isLogin = !this.isLogin;
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      this.http.add(this.loginForm.value).subscribe({
+        next: (res: any) => {
+          if (res.status_cd == 1) {
+
+          }
+          else if (res.status_cd == 0) {
+
+          }
+
+        }, error: (err: any) => {
+
+        }
+      })
+    }
+  }
+
+  onRegister() {
+    if (this.registerForm.valid) {
+      this.http.add(this.registerForm.value).subscribe({
+        next: (res: any) => {
+          if (res.status_cd == 1) {
+
+          } else if (res.status_cd == 0) {
+
+          }
+        },
+        error: (err: any) => {
+
+        }
+      });
+    }
+  }
 }
